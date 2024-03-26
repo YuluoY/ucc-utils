bf1f668941a267b8639c03f935ea4a6f78b0296b
Yuluo <568055454@qq.com>
2024-03-26 18:06:59 +0800

    update: init

---
+++ types/index.ts
@@ -0,0 +1,19 @@
+
+/**
+ * 获取一个类型中所有可选参数的类型
+ */
+export type GetOptionals<T extends object> = {
+    [K in keyof T as T[K] extends Required<T>[K] ? never : K]: T[K] 
+}
+
+/**
+ * 获取一个类型中所有必选参数的类型
+ */
+export type GetRequired<T extends object> = {
+    [K in keyof T as T[K] extends Required<T>[K] ? K : never]: T[K]
+}
+
+/**
+ * 指定获取类型中的属性
+ */
+export type GetProperty<T extends object, K extends keyof T> = T[K]
