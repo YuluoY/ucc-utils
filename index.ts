20104b239ba2151d10988e2683bd2c6cffa3c79d
huyongle <568055454@qq.com>
2024-08-03 05:26:16 +0800

    feat: 完成工具包的发布使用

--- index.ts
+++ index.ts
@@ -1,4 +1,3 @@
-import * as types from './src/types'
 import * as core from './src/core'
 import * as dom from './src/dom'
 import storage from './src/storage'
@@ -12,7 +11,6 @@ export {
   socket, // websocket
   worker, // web worker
   sql, // sql工具操作
-  types, // ts类型定义函数
   core, // 核心工具
   storage, // 本地存储
   request, // 请求类
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,39 @@
 import type { Options } from 'tsup'
+import fs from 'node:fs'
 
 export const tsup: Options = {
-  entry: ['./index.ts', './Polyfill.ts'],
+  entry: {
+    index: './index.ts',
+    'core/index': 'src/core/index.ts',
+    'dom/index': 'src/dom/index.ts',
+    'hooks/index': 'src/hooks/index.ts',
+    'request/index': 'src/request/index.ts',
+    'socket/index': 'src/socket/index.ts',
+    'sql/index': 'src/sql/index.ts',
+    'storage/index': 'src/storage/index.ts',
+    'worker/index': 'src/worker/index.ts'
+  },
   format: ['cjs', 'esm'],
   dts: true,
   splitting: true,
   clean: true,
-  minify: true
+  minify: true,
+  outDir: 'dist',
+  external: ['ucc-utils'],
+  esbuildOptions(opts) {
+    opts.alias = {
+      'ucc-utils/core': 'ucc-utils/dist/core',
+      'ucc-utils/dom': 'ucc-utils/dist/dom',
+      'ucc-utils/hooks': 'ucc-utils/dist/hooks',
+      'ucc-utils/request': 'ucc-utils/dist/request',
+      'ucc-utils/socket': 'ucc-utils/dist/socket',
+      'ucc-utils/sql': 'ucc-utils/dist/sql',
+      'ucc-utils/storage': 'ucc-utils/dist/storage',
+      'ucc-utils/worker': 'ucc-utils/dist/worker'
+    }
+  },
+  async onSuccess() {
+    // 将 src/types 拷贝到 dist 目录下
+    fs.cpSync('src/types', 'dist/types', { recursive: true })
+  }
 }
